f1cc46ab68466a5edc97d3f3d084533c766a10f6
shaoxcs <shao.xue@outlook.com>
2020-08-07 11:20:23 -0700

    Add skeleton for solution

---
+++ backend/Solution.java
@@ -0,0 +1,16 @@
+package backend;
+
+public class Solution {
+
+  public static String encrypt(String s) {
+
+    return "case result";
+  }
+
+  public static void main(String[] args) {
+    String input1 = "haveaniceday";
+    String input2 = "feedthedog";
+    System.out.println(Solution.encrypt(input1));
+    System.out.println(Solution.encrypt(input2));
+  }
+}
